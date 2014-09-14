f54ae0210bff45543f929f31e25c83da154ecb29
Robert Gregor <bob@orionipventures.com>
2014-09-13 23:36:09 -0400

    pending verification XML parsed into array added cache file relative date logic

--- src/modules/addons/enom_pro/includes/domain_widget.php
+++ src/modules/addons/enom_pro/includes/domain_widget.php
@@ -33,24 +33,6 @@ $stats = $this->getAccountStats();
 
 		<tr>
 			<td class="enom_stat_button">
-				<a class="btn btn-danger <?php echo( $stats['verification'] > 0 ? '' : 'disabled' ) ?>"
-					 data-tab="verification"
-					 href="<?php echo enom_pro::MODULE_LINK . '&action=get_domains&tab=verification'; ?>">
-					<?php echo $stats['verification']; ?>
-				</a>
-
-				<div class="enom_pro_loader small hidden"></div>
-			</td>
-			<td class="enom_stat_label">Pending Verification</td>
-		</tr>
-		<tr>
-			<td colspan="2">
-				<div id="enom_pro_expiring"></div>
-			</td>
-		</tr>
-
-		<tr>
-			<td class="enom_stat_button">
 				<a class="btn btn-warning <?php echo( $stats['expiring'] > 0 ? '' : 'disabled' ) ?>"
 					 data-tab="expiring"
 					 href="<?php echo enom_pro::MODULE_LINK . '&action=get_domains&tab=expiring'; ?>">
