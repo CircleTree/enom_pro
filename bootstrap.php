d229f0bafb49b5c913757789addaabf8fff024ad
Robert Gregor <bob@orionipventures.com>
2015-01-17 14:51:49 -0500

    fix relative whmcs path

--- bootstrap.php
+++ bootstrap.php
@@ -3,4 +3,5 @@ error_reporting(E_ALL);
 $resp = mysql_connect('localhost', 'root', 'root');
 mysql_select_db('whmcs_3');
 define('BOOTSTRAP', true);
+define('ROOTDIR', realpath(__DIR__) . '/src');
 require_once 'src/modules/addons/enom_pro/enom_pro.php';
