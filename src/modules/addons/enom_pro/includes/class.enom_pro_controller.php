a35c69e7b73f2cb15f923153a32f4bbb3e93ab16
Robert Gregor <bob@orionipventures.com>
2015-02-20 16:22:38 -0500

    improved handling of errors for the domain import page

--- src/modules/addons/enom_pro/includes/class.enom_pro_controller.php
+++ src/modules/addons/enom_pro/includes/class.enom_pro_controller.php
@@ -101,6 +101,7 @@ class enom_pro_controller {
 	}
 
 	protected function render_import_table() {
+		self::status_header(500);
 		ob_start();
 		require_once ENOM_PRO_INCLUDES . 'fragment_domain_import_table.php';
 		$contents = ob_get_contents();
@@ -109,10 +110,10 @@ class enom_pro_controller {
 			'html'       => $contents,
 			'cache_date' => $this->enom->get_domain_cache_date(),
 		);
+		self::status_header(200);
 		$this->send_json( $data );
 
 	}
-
 	protected function get_domain_whois() {
 		try {
 			$whois    = $this->enom->getWHOIS( $_REQUEST['domain'] );
@@ -649,5 +650,68 @@ class enom_pro_controller {
 
 		$this->send_json(enom_pro::get_clients());
 
+	}
+	public static function status_header ($statusCode) {
+		static $status_codes = null;
+
+		if ($status_codes === null) {
+			$status_codes = array (
+				100 => 'Continue',
+				101 => 'Switching Protocols',
+				102 => 'Processing',
+				200 => 'OK',
+				201 => 'Created',
+				202 => 'Accepted',
+				203 => 'Non-Authoritative Information',
+				204 => 'No Content',
+				205 => 'Reset Content',
+				206 => 'Partial Content',
+				207 => 'Multi-Status',
+				300 => 'Multiple Choices',
+				301 => 'Moved Permanently',
+				302 => 'Found',
+				303 => 'See Other',
+				304 => 'Not Modified',
+				305 => 'Use Proxy',
+				307 => 'Temporary Redirect',
+				400 => 'Bad Request',
+				401 => 'Unauthorized',
+				402 => 'Payment Required',
+				403 => 'Forbidden',
+				404 => 'Not Found',
+				405 => 'Method Not Allowed',
+				406 => 'Not Acceptable',
+				407 => 'Proxy Authentication Required',
+				408 => 'Request Timeout',
+				409 => 'Conflict',
+				410 => 'Gone',
+				411 => 'Length Required',
+				412 => 'Precondition Failed',
+				413 => 'Request Entity Too Large',
+				414 => 'Request-URI Too Long',
+				415 => 'Unsupported Media Type',
+				416 => 'Requested Range Not Satisfiable',
+				417 => 'Expectation Failed',
+				422 => 'Unprocessable Entity',
+				423 => 'Locked',
+				424 => 'Failed Dependency',
+				426 => 'Upgrade Required',
+				500 => 'Internal Server Error',
+				501 => 'Not Implemented',
+				502 => 'Bad Gateway',
+				503 => 'Service Unavailable',
+				504 => 'Gateway Timeout',
+				505 => 'HTTP Version Not Supported',
+				506 => 'Variant Also Negotiates',
+				507 => 'Insufficient Storage',
+				509 => 'Bandwidth Limit Exceeded',
+				510 => 'Not Extended'
+			);
+		}
+
+		if (isset($status_codes[$statusCode])) {
+			$status_string = $statusCode . ' ' . $status_codes[$statusCode];
+			header($_SERVER['SERVER_PROTOCOL'] . ' ' . $status_string, true, $statusCode);
+		}
 	}
 }
