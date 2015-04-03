657b374b0e7b4b6939b2bd5bffccf59a190bcc32
Robert Gregor <bob@orionipventures.com>
2015-04-03 13:19:51 -0400

    added whois server status filters

--- src/modules/addons/enom_pro/includes/page_whois_checker.php
+++ src/modules/addons/enom_pro/includes/page_whois_checker.php
@@ -11,6 +11,9 @@ if (! file_exists($filepath)) {
 }
 $file_contents = file_get_contents( $filepath );
 $file_array = explode(PHP_EOL, $file_contents);
+/**
+ * @var array tld => server address
+ */
 $parsed_whois_servers = array();
 foreach ($file_array as $line) {
 	$file_line = explode('|', $line);
@@ -23,22 +26,53 @@ $result = select_query('tbldomainpricing', 'extension');
 if (false === mysql_num_rows($result)) {
 	throw new Exception("No TLD's found in WHMCS. Please configure TLD's before checking for WHOIS servers");
 }
+function enom_pro_whois_show_only (){
+	if (! isset($_GET['show'])) {
+		return 'all';
+	}
+	if ('missing' == $_GET['show']) {
+		return 'missing';
+	}
+	if ('ok' == $_GET['show']) {
+		return 'ok';
+	}
+}
+function enom_pro_whois_render_row ($whoisOK, $message){ ?>
+	<div class="col-xs-3">
+		<div class="alert <?php echo $whoisOK ? 'alert-success' : 'alert-danger' ?>">
+			<?php echo $message; ?>
+		</div>
+	</div>
+<?php
+}
 ?>
+<a
+	class="btn btn-default <?php echo 'missing' == enom_pro_whois_show_only() ? 'active' : '' ?>"
+	href="<?php echo enom_pro::MODULE_LINK ?>&view=whois_checker&show=missing">Only Show Missing</a>
+<a
+	class="btn btn-default <?php echo 'ok' == enom_pro_whois_show_only() ? 'active' : '' ?>"
+	href="<?php echo enom_pro::MODULE_LINK ?>&view=whois_checker&show=ok">Only Show OK</a>
+<a
+	class="btn btn-default <?php echo 'all' == enom_pro_whois_show_only() ? 'active' : '' ?>"
+	href="<?php echo enom_pro::MODULE_LINK ?>&view=whois_checker">Show All</a>
 <div class="row">
 	<?php while ($row = mysql_fetch_assoc($result)) :
 		$thisTLD = $row['extension'];
 		$whoisOK = false;
 		if (array_key_exists( $thisTLD, $parsed_whois_servers)) {
-			$message = 'Whois OK for:' . $thisTLD;
+			$message = 'Using Server <code>'.$parsed_whois_servers[$thisTLD].'</code> for TLD <code>'.$thisTLD.'</code>';
 			$whoisOK = true;
 		} else {
 			$message = 'Missing WHOIS server for: ' . $thisTLD;
 		}
+		if ('all' == enom_pro_whois_show_only()) {
+			enom_pro_whois_render_row($whoisOK, $message);
+		} elseif ('missing' == enom_pro_whois_show_only() && false === $whoisOK) {
+			enom_pro_whois_render_row($whoisOK, $message);
+		} elseif ('ok' == enom_pro_whois_show_only() && true == $whoisOK) {
+			enom_pro_whois_render_row($whoisOK, $message);
+		}
 	?>
-		<div class="col-xs-3">
-			<div class="alert <?php echo $whoisOK ? 'alert-success' : 'alert-danger' ?>">
-				<?php echo $message; ?>
-			</div>
-		</div>
+
 	<?php endwhile; ?>
 </div>
