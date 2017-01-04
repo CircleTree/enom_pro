933ca29c5d4bc7ea25ad28ca6a8fde397b99be5c
Ronny Tomasetti <ronny.tomasetti@gmail.com>
2017-01-04 08:11:29 -0500

    Update gulp workflow and minor widget layout fix
    Wrapped widget content inside of div with enom_pro_widget class.
    This adds a bit of padding for the widget content.

--- gulpfile.js
+++ gulpfile.js
@@ -6,37 +6,51 @@
  */
 var gulp = require('gulp'),
 	run = require('gulp-run'),
+	wait = require('gulp-wait'),
+	less = require('gulp-less'),
 	livereload = require('gulp-livereload');
 
 /**
  * Project source directory
  */
-var src = './src';
+var src = './src',
+	src_css = src + '/modules/addons/enom_pro/css';
 
 /**
  * Task uses ssh to login into local development pv and auto
  * executes phing local target build inside of enom_pro directory.
- * Pipes results through livereload to refresh page.
  */
-gulp.task('run-phing', function() {
-	run("ssh vagrant@127.0.0.1 -p 2222 'cd enom_pro; vendor/bin/phing;'").exec()
+gulp.task('run-phing', function () {
+	return run("ssh vagrant@127.0.0.1 -p 2222 'cd enom_pro; vendor/bin/phing;'")
+		.exec()
 		.on('error', onError)
+		.pipe(wait(4000))
 		.pipe(livereload());
+});
+
+gulp.task('less', function () {
+	return gulp.src(src_css + '/admin.less')
+		.pipe(less({
+			paths: [src_css]
+		}))
+		.pipe(gulp.dest(src_css));
 });
 
 /**
  * Task to watch src directory for any changes refreshing page with livereload.
  * If changes are detected, executes 'run-phing' task to rebuild project.
  */
-gulp.task('watch', function() {
+gulp.task('watch', function () {
 	livereload.listen();
-	gulp.watch( src + '/**/*', ['run-phing'] );
+	gulp.watch(src + '/**/*', ['run-phing']);
+	gulp.watch(src_css + '/admin.less', ['less']);
+	return;
 });
 
 /**
  * Default gulp task runs when executing 'gulp'
  */
-gulp.task('default', ['watch', 'run-phing']);
+gulp.task('default', ['watch', 'less', 'run-phing']);
 
 /**
  * Error handler that keeps gulp from crashing when it encounters an error.
@@ -44,4 +58,5 @@ gulp.task('default', ['watch', 'run-phing']);
 function onError(err) {
 	console.log(err);
 	this.emit('end');
+	return;
 }
--- src/modules/addons/enom_pro/includes/widget_domain_pending_transfers.php
+++ src/modules/addons/enom_pro/includes/widget_domain_pending_transfers.php
@@ -35,84 +35,86 @@ function enom_pro_render_transfer_status_form( $domain, $status ) { ?>
 }
 
 ?>
-<div class="enomtransfers enom_pro_widget">
-	<?php if ( empty( $transfers ) ) : ?>
-		<?php //Exit early if no transfers ?>
-		<div class="alert alert-success enom_pro_widget">No pending transfers found in WHMCS</div>
-		<?php return; ?>
-	<?php endif; ?>
 
-	<table id="enom_pro_transfers_table">
-		<tr>
-			<th>Domain</th>
-			<th>WHMCS Domains</th>
-			<th>Orders</th>
-		</tr>
-		<?php foreach ( $transfers as $domain ) : ?>
+<div class="enom_pro_widget">
+	<div class="enomtransfers">
+		<?php if ( empty( $transfers ) ) : ?>
+			<?php //Exit early if no transfers ?>
+			<div class="alert alert-success">No pending transfers found in WHMCS</div>
+			<?php return; ?>
+		<?php endif; ?>
 
+		<table id="enom_pro_transfers_table">
 			<tr>
-				<td>
-					<?php //TODO possibly use WHMCS's whois.php if we can get a token to use on the $POST ?>
-					<a class="domain_name"
-					   target="_blank"
-					   title="View WHOIS"
-					   href="http://www.whois.net/whois/<?php echo $domain['domain']; ?>">
-						<?php echo $domain['domain']; ?>
-					</a>
-				</td>
-				<td style="text-align:center;">
-					<?php ob_start(); //Keep it DRY - re-use the edit button?>
-					<a
-						class="btn btn-default"
-						href="clientsdomains.php?userid=<?php echo $domain['userid'] ?>&id=<?php echo $domain['id'] ?>">
-						Edit
-					</a>
-					<?php $edit_domain_button = ob_get_contents(); ?>
-					<?php ob_end_flush(); ?>
-				</td>
-				<td>
-					<?php if ( 0 == count( $domain['statuses'] ) ) : ?>
-						<div class="alert alert-info">No Orders Found <?php echo $edit_domain_button; ?></div>
-					<?php else: ?>
-
-						<table class="none">
-							<tr>
-								<th>
-									eNom Order ID
-								</th>
-								<th>
-									Actions
-								</th>
-								<th class="center">
-									Description
-								</th>
-							</tr>
-							<?php //now we need to loop through the multiple order statuses
-							// returned for each domain order ?>
-							<?php foreach ( $domain['statuses'] as $status ) :
-								$status = (array) $status; ?>
-
-								<tr>
-									<td>
-										<a target="_blank"
-										   title="Order Date: <?php echo $status['orderdate']; ?>"
-										   href="https://www.enom.com/domains/TransferStatus.asp?transferorderid={$status['orderid']}">
-											<?php echo $status['orderid']; ?>
-										</a>
-									</td>
-									<td style="text-align:center;">
-										<?php enom_pro_render_transfer_status_form( $domain, $status ); ?>
-									</td>
-									<td>
-										<?php echo $status['statusdesc']; ?>
-									</td>
-								</tr>
-							<?php endforeach; ?>
-						</table>
-					<?php endif; ?>
-				</td>
+				<th>Domain</th>
+				<th>WHMCS Domains</th>
+				<th>Orders</th>
 			</tr>
-		<?php endforeach; ?>
-	</table>
-</div>
+			<?php foreach ( $transfers as $domain ) : ?>
+
+				<tr>
+					<td>
+						<?php //TODO possibly use WHMCS's whois.php if we can get a token to use on the $POST ?>
+						<a class="domain_name"
+						   target="_blank"
+						   title="View WHOIS"
+						   href="http://www.whois.net/whois/<?php echo $domain['domain']; ?>">
+							<?php echo $domain['domain']; ?>
+						</a>
+					</td>
+					<td style="text-align:center;">
+						<?php ob_start(); //Keep it DRY - re-use the edit button?>
+						<a
+							class="btn btn-default"
+							href="clientsdomains.php?userid=<?php echo $domain['userid'] ?>&id=<?php echo $domain['id'] ?>">
+							Edit
+						</a>
+						<?php $edit_domain_button = ob_get_contents(); ?>
+						<?php ob_end_flush(); ?>
+					</td>
+					<td>
+						<?php if ( 0 == count( $domain['statuses'] ) ) : ?>
+							<div class="alert alert-info">No Orders Found <?php echo $edit_domain_button; ?></div>
+						<?php else: ?>
+
+							<table class="none">
+								<tr>
+									<th>
+										eNom Order ID
+									</th>
+									<th>
+										Actions
+									</th>
+									<th class="center">
+										Description
+									</th>
+								</tr>
+								<?php //now we need to loop through the multiple order statuses
+								// returned for each domain order ?>
+								<?php foreach ( $domain['statuses'] as $status ) :
+									$status = (array) $status; ?>
+
+									<tr>
+										<td>
+											<a target="_blank"
+											   title="Order Date: <?php echo $status['orderdate']; ?>"
+											   href="https://www.enom.com/domains/TransferStatus.asp?transferorderid={$status['orderid']}">
+												<?php echo $status['orderid']; ?>
+											</a>
+										</td>
+										<td style="text-align:center;">
+											<?php enom_pro_render_transfer_status_form( $domain, $status ); ?>
+										</td>
+										<td>
+											<?php echo $status['statusdesc']; ?>
+										</td>
+									</tr>
+								<?php endforeach; ?>
+							</table>
+						<?php endif; ?>
+					</td>
+				</tr>
+			<?php endforeach; ?>
+		</table>
+	</div>
 </div>
